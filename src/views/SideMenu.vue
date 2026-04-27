1aa6169eaa291d70a14eb01a310502a0d4a0612f
Jonathan Snyder <jon.jsstudios@gmail.com>
2026-04-27 20:51:31 +0700

    fix sidenav

--- src/views/SideMenu.vue
+++ src/views/SideMenu.vue
@@ -7,7 +7,7 @@
         href="#"
         data-toggle="modal"
         data-target="#modal-shortcut"
-        class="page-logo-link press-scale-down flex items-center relative w-64"
+        class="page-logo-link press-scale-down flex items-center relative w-64 p-5"
         ><img
           src="@/assets/img/wrecking-ball-logo-light-bg.webp"
           alt="Wreckingball Labs"
@@ -16,9 +16,9 @@
         <i class="fal fa-angle-down inline-block ml-1 fs-lg color-primary-300"></i
       ></a>
     </div>
-    <nav id="js-primary-nav" role="navigation" class="primary-nav">
+    <nav id="js-primary-nav" role="navigation" class="primary-nav p-5">
       <ul id="js-nav-menu" class="nav-menu" style="margin-left: 0px">
-        <li>hey</li>
+        <li>Physics</li>
       </ul>
     </nav>
   </aside>
